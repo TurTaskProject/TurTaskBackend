13df2e8cb8e929a508240cf965a75fec6fa3f330
sosokker <sirin_29@hotmail.co.th>
2023-10-31 04:48:06 +0700

    Add Navbar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { GoogleLogin, GoogleOAuthProvider} from '@react-oauth/google';
+import { GoogleOAuthProvider} from '@react-oauth/google';
 
 const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-    <App />
+      <App />
   </GoogleOAuthProvider>
 );
