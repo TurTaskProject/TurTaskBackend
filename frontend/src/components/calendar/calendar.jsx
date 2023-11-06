074aeb70bc64fbdf8a1e61bafd43904fe9ab00e7
Wissarut Kanasub <wissarut.dev@gmail.com>
2023-11-06 11:08:44 +0700

    Calander component and branch

---
+++ frontend/src/components/calendar/calendar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+
+const Calendar = () => {
+  return (
+    <div className="App">
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin]}
+        initialView="dayGridMonth"
+        events={[
+          { title: 'Event 1', date: '2023-11-10' },
+          { title: 'Event 2', date: '2023-11-15' },
+          // Add more events as needed
+        ]}
+        headerToolbar={{
+          start: 'prev,next',
+          center: "title",
+          end: 'timeGridDay,dayGridMonth,timeGridWeek,',
+          
+        }}
+      />
+    </div>
+  );
+};
+
+export default Calendar;
