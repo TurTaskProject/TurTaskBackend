1b436d2dc6e125e63e036ee067d3faa284c24da3
sosokker <sirin_29@hotmail.co.th>
2023-11-09 23:52:33 +0700

    Add RecurrenceTaskViewSet

--- backend/tasks/tasks/views.py
+++ backend/tasks/tasks/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from tasks.models import Todo
+from tasks.models import Todo, RecurrenceTask
 from .serializers import TaskCreateSerializer, TaskGeneralSerializer
 
 
@@ -14,3 +14,9 @@ class TodoViewSet(viewsets.ModelViewSet):
         if self.action == 'create':
             return TaskCreateSerializer
         return TaskGeneralSerializer
+    
+
+class RecurrenceTaskViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TaskGeneralSerializer
+    permission_classes = [IsAuthenticated]
