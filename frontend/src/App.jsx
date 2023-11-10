0eaa99f9a95ce78f14961a58b1538507d1779f72
Pattadon <pattadon2546pn@gmail.com>
2023-11-10 13:57:10 +0700

    Completed kanban system.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import TestAuth from './components/testAuth';
 import LoginPage from './components/authentication/LoginPage';
@@ -9,24 +9,24 @@ import Home from './components/Home';
 import ProfileUpdate from './components/ProfileUpdatePage';
 import Calendar from './components/calendar/calendar';
 import KanbanBoard from './components/kanbanBoard/kanbanBoard';
-
+import IconSideNav from './components/IconSideNav'; // Import IconSideNav
 
 const App = () => {
     return (
         <BrowserRouter>
-        <div className="App">
-                    <NavBar/>
-                    <Routes>
-                        <Route path="/" element={<Home/>}/>
-                        <Route path="/login" element={<LoginPage/>}/>
-                        <Route path="/signup" element={<SignUpPage/>}/>
-                        <Route path="/testAuth" element={<TestAuth/>}/>
-                        <Route path="/update_profile" element={<ProfileUpdate/>}/>
-                        <Route path="/calendar" element={<Calendar/>}/>
-                    </Routes>
-        </div>
-        {/* <IconSideNav /> */}
-        <KanbanBoard/>  
+            <div className="App">
+                <NavBar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/signup" element={<SignUpPage />} />
+                    <Route path="/testAuth" element={<TestAuth />} />
+                    <Route path="/update_profile" element={<ProfileUpdate />} />
+                    <Route path="/calendar" element={<Calendar />} />
+                </Routes>
+            </div>
+            <IconSideNav /> {<KanbanBoard />}
+            
         </BrowserRouter>
     );
 }
