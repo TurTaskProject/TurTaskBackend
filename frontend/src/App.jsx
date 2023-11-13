c8ddf9519789b3372b85af5f35a6e6fb57d87748
Pattadon <pattadon2546pn@gmail.com>
2023-11-13 12:13:27 +0700

    Changed folder name.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import TestAuth from "./components/testAuth";
 import LoginPage from "./components/authentication/LoginPage";
 import SignUpPage from "./components/authentication/SignUpPage";
-import NavBar from "./components/Nav/Navbar";
+import NavBar from "./components/navigators/Navbar";
 import Home from "./components/Home";
 import ProfileUpdate from "./components/ProfileUpdatePage";
 import Calendar from "./components/calendar/calendar";
