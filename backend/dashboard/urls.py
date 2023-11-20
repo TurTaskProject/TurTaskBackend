ae892545293d0dc5e89e481a467a6d5a0681aee4
Chaiyawut Thengket <santa081831@gmail.com>
2023-11-20 10:47:54 +0700

    write weekly urls

--- backend/dashboard/urls.py
+++ backend/dashboard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import DashboardStatsAPIView
+from .views import DashboardStatsAPIView, WeeklyStatsAPIView
 
 urlpatterns = [
     path('dashboard/stats/', DashboardStatsAPIView.as_view(), name='dashboard-stats'),
+    path('dashboard/weekly-stats/', WeeklyStatsAPIView.as_view(), name='dashboard-weekly-stats'),
 ]
