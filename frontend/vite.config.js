84bd04126e7f081c265bab17881db68058e0eb8c
sosokker <sirin_29@hotmail.co.th>
2023-11-22 22:47:16 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       src: "/src",
     },
+  },
+  define: {
+    __APP_ENV__: import.meta.env.VITE_VERCEL_ENV,
   },
 });
