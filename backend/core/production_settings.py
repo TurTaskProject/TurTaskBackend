d98c77acc62b0d00cf87a0aa40e09ab12dd724b0
sosokker <sirin_29@hotmail.co.th>
2023-11-22 22:55:49 +0700

    Change CORS

--- backend/core/production_settings.py
+++ backend/core/production_settings.py
@@ -121,12 +121,10 @@ SOCIALACCOUNT_PROVIDERS = {
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-    "http://localhost:5173",
+    "https://turtask.vercel.app",
 ]
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost:5173"]
+CSRF_TRUSTED_ORIGINS = ["https://turtask.vercel.app"]
 
 CORS_ORIGIN_WHITELIST = ["*"]
 
