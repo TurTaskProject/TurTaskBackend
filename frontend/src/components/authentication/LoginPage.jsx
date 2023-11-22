afbd654c37b3328220ea56cd8136faaede0b8639
Sirin Puenggun <sirin_29@hotmail.co.th>
2023-11-22 16:10:32 +0700

    Update LoginPage.jsx

--- frontend/src/components/authentication/LoginPage.jsx
+++ frontend/src/components/authentication/LoginPage.jsx
@@ -4,7 +4,7 @@ import { useGoogleLogin } from "@react-oauth/google";
 import { useCallback } from "react";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
-import refreshAccessToken from "./refreshAcesstoken";
+import refreshAccessToken from "./refreshAcessToken";
 import axiosapi from "../../api/AuthenticationApi";
 import { useAuth } from "../../hooks/authentication/IsAuthenticated";
 import { FcGoogle } from "react-icons/fc";
