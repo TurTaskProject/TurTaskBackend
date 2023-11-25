f6e9489a14e9b43b2d1fe02d93b39858af7ef6b9
Chaiyawut Thengket <santa081831@gmail.com>
2023-11-25 13:17:13 +0700

    add new url

--- backend/dashboard/urls.py
+++ backend/dashboard/urls.py
@@ -4,9 +4,9 @@ from rest_framework.routers import DefaultRouter
 from .views import DashboardStatsTodoViewSet, DashboardWeeklyViewSet
 
 router = DefaultRouter()
-router.register(r'dashboard/stats', DashboardStatsTodoViewSet, basename='statstodo')
+router.register(r'dashboard/todostats', DashboardStatsTodoViewSet, basename='statstodo')
 router.register(r'dashboard/weekly', DashboardWeeklyViewSet, basename='weekly')
-router.register(r'dashboard/stats', DashboardStatsTodoViewSet, basename='statsrec')
+router.register(r'dashboard/recstats', DashboardStatsTodoViewSet, basename='statsrec')
 urlpatterns = [
     path('', include(router.urls)),
 ]
