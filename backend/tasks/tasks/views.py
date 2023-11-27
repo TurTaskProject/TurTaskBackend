97e4cc5a0dae8e34039a60f766968f37892e5620
sosokker <sirin_29@hotmail.co.th>
2023-11-28 01:26:40 +0700

    Fix subtask viewset typo error

--- backend/tasks/tasks/views.py
+++ backend/tasks/tasks/views.py
@@ -177,7 +177,7 @@ class SubTaskViewset(viewsets.GenericViewSet,
     def partial_update(self, request, *args, **kwargs):
         """Update a subtask."""
         try:
-            instance = self.get_o
+            instance = self.get_object()
             serializer = self.get_serializer(instance, data=request.data, partial=True)
             serializer.is_valid(raise_exception=True)
             self.perform_update(serializer)
