fcff22e64dfb0e54a3e4ed729bc04f3b9cc3edc7
sosokker <sirin_29@hotmail.co.th>
2023-11-27 14:17:37 +0700

    Add user data retreival viewset

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from users.views import CustomUserCreate, CustomUserProfileUpdate
+from users.views import CustomUserCreate, CustomUserProfileUpdate, UserDataRetriveViewset
 
 urlpatterns = [
     path('user/create/', CustomUserCreate.as_view(), name="create_user"),
-    path('user/update/', CustomUserProfileUpdate.as_view(), name='update_user')
+    path('user/update/', CustomUserProfileUpdate.as_view(), name='update_user'),
+    path('user/data/', UserDataRetriveViewset.as_view({'get': 'retrieve'}), name="get_user_data"),
 ]
