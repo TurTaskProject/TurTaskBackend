3c74d43f4921144e0644d59b16222ec080c7c6ae
Chaiyawut Thengket <santa081831@gmail.com>
2023-11-27 15:52:13 +0700

    add field

--- backend/users/views.py
+++ backend/users/views.py
@@ -63,7 +63,7 @@ class CustomUserProfileUpdate(APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    
+
 class UserDataRetriveViewset(viewsets.GenericViewSet, mixins.RetrieveModelMixin):
     queryset = CustomUser.objects.all()
     permission_classes = (IsAuthenticated,)
